876b75e089f78abd2c8c0eae6778e5a59bceb1c5
Adelemphii <demilitioowner@gmail.com>
2021-11-11 06:27:56 -0600

    Initial commit

---
+++ src/main/java/me/adelemphii/kalliergeia/Kalliergeia.java
@@ -0,0 +1,24 @@
+package me.adelemphii.kalliergeia;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Kalliergeia extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        registerCommands();
+        registerEvents();
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    private void registerCommands() {
+
+    }
+
+    private void registerEvents() {
+
+    }
+}
